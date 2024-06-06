d8a455ace49c1bc88f00a78911b2319200ea909f
HUYNHTV2711 <huynhptit2711@gmail.com>
2024-06-06 10:28:50 +0700

    update token

--- src/services/configServ.js
+++ src/services/configServ.js
@@ -6,7 +6,7 @@ export const https =  axios.create({
     baseURL: "https://elearningnew.cybersoft.edu.vn",
     headers: {
         Authorization :`Bearer ${dataUser ? dataUser.accessToken : ""}`,
-        TokenCybersoft: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCBTw6FuZyAwOSIsIkhldEhhblN0cmluZyI6IjMwLzA1LzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcxNzAyNzIwMDAwMCIsIm5iZiI6MTY5MjI5MTYwMCwiZXhwIjoxNzE3MTc0ODAwfQ.7MW8E_eXXd0bcbNFchNRQTlWpRBVvM0yUAkLRSo12ws',
+        TokenCybersoft: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJOb2RlSlMgMzgiLCJIZXRIYW5TdHJpbmciOiIxNC8wNy8yMDI0IiwiSGV0SGFuVGltZSI6IjE3MjA5MTUyMDAwMDAiLCJuYmYiOjE3MDI0ODY4MDAsImV4cCI6MTcyMTA2MjgwMH0.cB5XSbdlq0lzL-wmbcuAyvlRLMYFWmr20ODRWN5rPZc',
     },
     timeout: 15000,
 })
